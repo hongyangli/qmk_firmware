8eea21f7043f811d029c3ce6528eec4aceb377d5
Hongyang Li <hongyangli@fb.com>
2017-05-16 15:07:42 -0700

    update

--- keyboards/preonic/keymaps/hongyang/keymap.c
+++ keyboards/preonic/keymaps/hongyang/keymap.c
@@ -32,19 +32,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Ctrl |
+ * | Shift|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Shift|
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
+ * | Lower|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Raise|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | GUI  |   \  | GUI  |  Alt | LOWER| Enter| Space| RAISE|  Alt |  GUI |   '  | GUI  |
+ * | GUI  |   \  | GUI  |  Alt | Ctrl | Enter| Space| Ctrl |  Alt |  GUI |   '  | GUI  |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV},
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RCTL},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT},
-  {KC_LGUI, KC_BSLS, KC_LGUI, KC_LALT, LOWER,  KC_ENT,  KC_SPC,   RAISE,   KC_RALT, KC_RGUI, KC_QUOT, KC_RGUI},  
+  {KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT},
+  {LOWER,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RAISE},
+  {KC_LGUI, KC_BSLS, KC_LGUI, KC_LALT, KC_LCTL, KC_ENT,  KC_SPC,  KC_RCTL, KC_RALT, KC_RGUI, KC_QUOT, KC_RGUI},  
 },
 
 /* Lower
