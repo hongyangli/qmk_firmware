0b50d68cf7b50f31ca49ea96b65881ad45f27364
Hongyang Li <hongyangli@fb.com>
2017-07-14 10:45:55 -0700

    update

--- keyboards/planck/keymaps/hongyang/keymap.c
+++ keyboards/planck/keymaps/hongyang/keymap.c
@@ -17,16 +17,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Ctrl |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
+ * | Alt  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Alt  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | GUI  |  Alt | Left |Right |Lower |    Space    |Raise | Up   | Down |  Alt | GUI  |
+ * | GUI  | Left |Right |Shift |Lower |    Space    |Raise |Shift |  Up  | Down | GUI  |
  * `-----------------------------------------------------------------------------------'
  */
 [0] = {
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT},
   {KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RCTL},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT },
-  {KC_LGUI, KC_LALT, KC_LEFT, KC_RGHT, M(1),    KC_SPC,  KC_SPC,   M(2),   KC_UP,   KC_DOWN, KC_RALT, KC_RGUI}
+  {KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT},
+  {KC_LGUI, KC_LEFT, KC_RGHT, KC_LSFT, M(1),    KC_SPC,  KC_SPC,  M(2),    KC_RSFT, KC_UP,   KC_DOWN, KC_RGUI}
 },
 
 /* Lower
